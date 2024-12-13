8e9ebc728daf7c92925f848ccdfd5676e6c4b5d1
Catking14 <edarhinos9@gmail.com>
2024-12-13 04:00:53 +0000

    further reduce (no help)

--- jac_solv_sycl.cpp
+++ jac_solv_sycl.cpp
@@ -48,6 +48,19 @@ extern SYCL_EXTERNAL int rand(void);
 #define DEBUG    1     // output a small subset of intermediate values
 // #define VERBOSE  1
 
+int round_up_power_of_2(int v)
+{
+	v--;
+	v |= v >> 1;
+	v |= v >> 2;
+	v |= v >> 4;
+	v |= v >> 8;
+	v |= v >> 16;
+	v++;
+
+	return v;
+}
+
 void init_diag_dom_near_identity_matrix_sycl(int Ndim, TYPE *A, sycl::id<1> idx, int size)
 {
 
@@ -79,22 +92,37 @@ void init_diag_dom_near_identity_matrix_sycl(int Ndim, TYPE *A, sycl::id<1> idx,
 	}
 }
 
-void jalc_solver_main(sycl::nd_item<1> item, int Ndim, TYPE* A, TYPE* b, TYPE* d_xnew, TYPE* d_xold, TYPE* d_conv, TYPE* d_conv_reduce)
+void jac_solver_first(sycl::nd_item<1> item, int Ndim, int Ndim_round, TYPE* A, TYPE* d_xold, TYPE* A_reduce, int world_size)
+{
+	// blockIdx.x * blockDim.x + threadIdx.x
+	int rank = item.get_group(0) * item.get_local_range().get(0) + item.get_local_id(0);
+	TYPE test;
+
+	// out << rank << " " << world_size << " " << Ndim << " " << Ndim_round << "\n";
+
+	// *d_conv = 0.0;
+	for (int idx = rank; idx < Ndim * Ndim; idx += world_size)
+	{
+		int i = idx / Ndim;
+		int j = idx % Ndim;
+
+		// d_xnew[idx] = 0.0;
+
+		if (i != j)
+			A_reduce[i * Ndim_round + j] = A[i * Ndim + j] * d_xold[j];
+		else
+			A_reduce[i * Ndim_round + i] = 0;
+	}
+}
+
+void jac_solver_last(sycl::nd_item<1> item, int Ndim, TYPE* A, TYPE* b, TYPE* d_xnew, TYPE* d_xold, TYPE* d_conv_reduce)
 {
 	// blockIdx.x * blockDim.x + threadIdx.x
 	int idx = item.get_group(0) * item.get_local_range().get(0) + item.get_local_id(0);
 	TYPE tmp;
 
-	// *d_conv = 0.0;
 	if (idx < Ndim)
 	{
-		d_xnew[idx] = 0.0;
-
-		for (int j = 0; j < Ndim; j++)
-		{
-			if (idx != j)
-				d_xnew[idx] += A[idx * Ndim + j] * d_xold[j];
-		}
 		d_xnew[idx] = (b[idx] - d_xnew[idx]) / A[idx * Ndim + idx];
 
 		// test convergence
@@ -111,9 +139,9 @@ void jalc_solver_main(sycl::nd_item<1> item, int Ndim, TYPE* A, TYPE* b, TYPE* d
 	d_conv_reduce[item.get_group(0)] = sycl::reduce_over_group(item.get_group(), tmp, sycl::plus<TYPE>());
 }
 
-void reduce(sycl::nd_item<1> item, TYPE *d_conv, TYPE *d_conv_reduce, int size)
+void reduce(sycl::nd_item<1> item, TYPE *d_val, TYPE *d_arr_reduce, int size)
 {
-	// reduce d_conv_reduce
+	// reduce d_arr_reduce
 	int idx = item.get_local_id(0);
 	int cal_size = size / 2;
 
@@ -121,44 +149,54 @@ void reduce(sycl::nd_item<1> item, TYPE *d_conv, TYPE *d_conv_reduce, int size)
 	{
 		if (idx < cal_size)
 		{
-			d_conv_reduce[idx] += d_conv_reduce[idx + cal_size];
+			d_arr_reduce[idx] += d_arr_reduce[idx + cal_size];
 		}
 		cal_size /= 2;
 
 		sycl::group_barrier(item.get_group());
 	}
 
-	if (idx == 0) d_conv[0] = d_conv_reduce[0];
+	if (idx == 0) d_val[0] = d_arr_reduce[0];
+}
 
-	// if (idx == 0)
-	// {
-	// 	d_conv[0] = 0;
+void reduce_A(sycl::nd_item<1> item, int i, TYPE *d_val, TYPE *d_arr_reduce, int size)
+{
+	// reduce d_arr_reduce
+	int idx = item.get_local_id(0);
+	int cal_size = size / 2;
 
-	// 	for (int i = 0; i < size; i++)
-	// 	{
-	// 		out << "d_conv_reduce[" << i << "] = " << d_conv_reduce[i] << "\n";
-	// 		d_conv[0] += d_conv_reduce[i];
-	// 	}
+	while (cal_size > 0)
+	{
+		if (idx < cal_size)
+		{
+			d_arr_reduce[i * size + idx] += d_arr_reduce[i * size + idx + cal_size];
+		}
+		cal_size /= 2;
 
-	// 	out << "d_conv[0] = " << d_conv[0] << "\n";
-	// }
+		sycl::group_barrier(item.get_group());
+	}
+
+	if (idx == 0) d_val[i] = d_arr_reduce[i * size];
 }
 
 int main(int argc, char **argv)
 {
-	int Ndim; // A[Ndim][Ndim]
+	int Ndim, Ndim_round; // A[Ndim][Ndim]
 	int iters;
 	double start_time, elapsed_time;
 	TYPE err, chksum;
 	TYPE *A, *b, *xnew, *xold;
-	TYPE *d_A, *d_b, *d_xnew, *d_xold;
+	TYPE *d_A, *d_A_reduce, *d_b, *d_xnew, *d_xold;
 
 	// cpu queue
 	// sycl::queue q;
 	// std::cout << "Running on " << q.get_device().get_info<sycl::info::device::name>() << "\n";
 
 	// gpu queue
-	sycl::queue q(sycl::gpu_selector_v, sycl::property::queue::in_order{});
+	sycl::device d(sycl::gpu_selector_v);
+	sycl::context c(d);
+	sycl::queue q(c, d, sycl::property::queue::in_order{});
+	sycl::queue q2(c, d);
 	std::cout << "Running on " << q.get_device().get_info<sycl::info::device::name>() << "\n";
 
 	// start timing
@@ -174,7 +212,9 @@ int main(int argc, char **argv)
 		Ndim = DEF_SIZE;
 	}
 
+	Ndim_round = round_up_power_of_2(Ndim);
 	printf(" ndim = %d\n", Ndim);
+	printf(" ndim Round = %d\n", Ndim_round);
 
 	A = (TYPE *)malloc(Ndim * Ndim * sizeof(TYPE));
 	b = (TYPE *)malloc(Ndim * sizeof(TYPE));
@@ -187,6 +227,7 @@ int main(int argc, char **argv)
 
 	// malloc on device
 	d_A = sycl::malloc_device<TYPE>(Ndim * Ndim, q);
+	d_A_reduce = sycl::malloc_device<TYPE>(Ndim_round * Ndim_round, q);
 	d_b = sycl::malloc_device<TYPE>(Ndim, q);
 	d_xnew = sycl::malloc_device<TYPE>(Ndim, q);
 	d_xold = sycl::malloc_device<TYPE>(Ndim, q);
@@ -242,24 +283,10 @@ int main(int argc, char **argv)
 	TYPE conv = LARGE;
 	int threads_per_block = 256;
 	int blocks_per_grid = ceil(Ndim / (TYPE) threads_per_block);
-	int reduce_blocks_per_grid = blocks_per_grid;
-
-	// round up to the nearest power of 2
-	reduce_blocks_per_grid--;
-	reduce_blocks_per_grid |= reduce_blocks_per_grid >> 1;
-	reduce_blocks_per_grid |= reduce_blocks_per_grid >> 2;
-	reduce_blocks_per_grid |= reduce_blocks_per_grid >> 4;
-	reduce_blocks_per_grid |= reduce_blocks_per_grid >> 8;
-	reduce_blocks_per_grid |= reduce_blocks_per_grid >> 16;
-	reduce_blocks_per_grid++;
+	int reduce_blocks_per_grid = round_up_power_of_2(blocks_per_grid);	// round up to the nearest power of 2
 
 	std::cout << "blocks_per_grid: " << blocks_per_grid << " " << "reduce_blocks_per_grid: " << reduce_blocks_per_grid << "\n";
 
-	// sycl::buffer<TYPE> A_buf(A, sycl::range<1>(Ndim * Ndim));
-    // sycl::buffer<TYPE> b_buf(b, sycl::range<1>(Ndim));
-    // sycl::buffer<TYPE> xold_buf(xold, sycl::range<1>(Ndim));
-    // sycl::buffer<TYPE> xnew_buf(xnew, sycl::range<1>(Ndim));
-    // sycl::buffer<TYPE> conv_buf(&conv, sycl::range<1>(1));
 	TYPE *d_conv = sycl::malloc_device<TYPE>(1, q);
 	TYPE *d_conv_reduce = sycl::malloc_device<TYPE>(reduce_blocks_per_grid, q);
 	sycl::range<1> blocks(blocks_per_grid);
@@ -267,6 +294,7 @@ int main(int argc, char **argv)
 
 	q.memset(d_conv, LARGE, sizeof(TYPE));
 	q.memset(d_conv_reduce, 0, sizeof(TYPE) * reduce_blocks_per_grid);
+	q.memset(d_A_reduce, 0, Ndim_round * Ndim_round * sizeof(TYPE));
 	iters = 0;
 
 	while ((conv > TOLERANCE) && (iters < MAX_ITERS))
@@ -281,15 +309,57 @@ int main(int argc, char **argv)
 			q.submit([&](sycl::handler &h)
 			{
 				// sycl::stream out(1024, 256, h); //output buffer
-				// sycl::accessor A_acc(A_buf, h);
-				// sycl::accessor b_acc(b_buf, h);
-				// sycl::accessor xold_acc(xold_buf, h);
-				// sycl::accessor xnew_acc(xnew_buf, h);
-				// sycl::accessor conv_acc(conv_buf, h);
 
 				h.parallel_for(sycl::nd_range<1>(blocks * threads, threads), [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]]
 				{	
-					jalc_solver_main(item, Ndim, d_A, d_b, d_xnew, d_xold, d_conv, d_conv_reduce);
+					// jalc_solver_main(item, Ndim, d_A, d_b, d_xnew, d_xold, d_conv, d_conv_reduce);
+					jac_solver_first(item, Ndim, Ndim_round, d_A, d_xold, d_A_reduce, blocks_per_grid * threads_per_block);
+
+					// out << d_A_reduce[0] << "\n";
+				}); 
+			});
+
+			// q.wait();
+
+			
+			q.submit([&](sycl::handler &h)
+			{
+				// sycl::stream out(1024, 256, h); //output buffer
+
+				h.parallel_for(sycl::nd_range<1>(Ndim_round / 2, Ndim_round / 2), [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]]
+				{	
+					for (int i = 0; i < Ndim; i++)
+						reduce(item, d_xnew + i, d_A_reduce + i * Ndim_round, Ndim_round);
+				}); 
+			});
+			
+
+			// q.submit([&](sycl::handler &h)
+			// {
+			// 	sycl::stream out(1024, 256, h); //output buffer
+
+			// 	h.parallel_for(sycl::nd_range<1>(Ndim, Ndim), [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]]
+			// 	{	
+			// 		int idx = item.get_local_id(0);
+			// 		d_xnew[idx] = 0.0;
+
+			// 		for (int i = 0; i < Ndim; i++)
+			// 		{
+			// 			d_xnew[idx] += d_A_reduce[idx * Ndim_round + i];
+			// 			if (idx == 0) out << d_xnew[idx] << "\n";
+			// 		}
+			// 	}); 
+			// });
+
+			// q2.wait();	// wait for all reduction over
+
+			q.submit([&](sycl::handler &h)
+			{
+				// sycl::stream out(1024, 256, h); //output buffer
+
+				h.parallel_for(sycl::nd_range<1>(blocks * threads, threads), [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]]
+				{	
+					jac_solver_last(item, Ndim, d_A, d_b, d_xnew, d_xold, d_conv_reduce);
 				}); 
 			});
 
@@ -308,15 +378,50 @@ int main(int argc, char **argv)
 			q.submit([&](sycl::handler &h)
 			{
 				// sycl::stream out(1024, 256, h); //output buffer
-				// sycl::accessor A_acc(A_buf, h);
-				// sycl::accessor b_acc(b_buf, h);
-				// sycl::accessor xold_acc(xold_buf, h);
-				// sycl::accessor xnew_acc(xnew_buf, h);
-				// sycl::accessor conv_acc(conv_buf, h);
 
 				h.parallel_for(sycl::nd_range<1>(blocks * threads, threads), [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]]
 				{	
-					jalc_solver_main(item, Ndim, d_A, d_b, d_xold, d_xnew, d_conv, d_conv_reduce);
+					// jalc_solver_main(item, Ndim, d_A, d_b, d_xold, d_xnew, d_conv, d_conv_reduce);
+					jac_solver_first(item, Ndim, Ndim_round, d_A, d_xnew, d_A_reduce, blocks_per_grid * threads_per_block);
+				}); 
+			});
+
+			// q.wait();
+
+			q.submit([&](sycl::handler &h)
+			{
+				// sycl::stream out(1024, 256, h); //output buffer
+
+				h.parallel_for(sycl::nd_range<1>(Ndim_round / 2, Ndim_round / 2), [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]]
+				{	
+					for (int i = 0; i < Ndim; i++)
+						reduce(item, d_xold + i, d_A_reduce + i * Ndim_round, Ndim_round);
+				}); 
+			});
+
+			// q.submit([&](sycl::handler &h)
+			// {
+			// 	h.parallel_for(sycl::nd_range<1>(Ndim, Ndim), [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]]
+			// 	{	
+			// 		int idx = item.get_local_id(0);
+			// 		d_xold[idx] = 0.0;
+
+			// 		for (int i = 0; i < Ndim; i++)
+			// 		{
+			// 			d_xold[idx] += d_A_reduce[idx * Ndim_round + i];
+			// 		}
+			// 	}); 
+			// });
+
+			// q2.wait();	// wait for all reduction over
+
+			q.submit([&](sycl::handler &h)
+			{
+				// sycl::stream out(1024, 256, h); //output buffer
+
+				h.parallel_for(sycl::nd_range<1>(blocks * threads, threads), [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(32)]]
+				{	
+					jac_solver_last(item, Ndim, d_A, d_b, d_xold, d_xnew, d_conv_reduce);
 				}); 
 			});
 
@@ -363,6 +468,8 @@ int main(int argc, char **argv)
 		// TYPE* tmp = xold;
 		// xold = xnew;
 		// xnew = tmp;
+
+		// if (iters == 2)	break;
 	}
 	// elapsed_time = omp_get_wtime() - start_time;
 	printf(" Convergence = %g with %d iterations and %f seconds\n", (float)conv,
